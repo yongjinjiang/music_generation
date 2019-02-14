269b337d07ef9db5062e217d261805fd77c8dfa6
jyj <yjjiangphysics@gmail.com>
2019-02-14 16:46:40 -0600

    add some mp3

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ import os
 import glob
 types1 = ('*.pdf', '*.py','*.mp3') # the tuple of file types 
 types2 =('*.mp3',)
+our_list=['StarWars100.mp3', 'StarWars1000SelfTrained.mp3', 'Simpons1000.mp3',\
+    'SimpsonsOriginal.mp3',  'StarWars1000.mp3', \
+       'TwoFountainsOriginal.mp3', 'TwoFountains1000.mp3', \
+           'Simpons100.mp3',  'TwoFountains100.mp3']
 #https://stackabuse.com/python-list-files-in-a-directory/
 
 app = Flask(__name__)
@@ -20,6 +24,8 @@ def index():
 # def upload():
 #    return render_template('upload.html')
 	
+
+
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_file():
    if request.method == 'POST':
@@ -39,10 +45,35 @@ def upload_file():
       for files in types2:
          files_grabbed2.extend(glob.glob("./static/"+files))
       files_grabbed2=[file.split('/')[-1] for file in files_grabbed2]
+      files_grabbed2=list(set(files_grabbed2)-set(our_list))
       print("files_grabbed2=",files_grabbed2) 
 
       return render_template('index.html', x1=files_grabbed1,x2=files_grabbed2)   # list files, delete files , and most importantly, do traning and generating new music!
 		
+
+# @app.route('/train', methods = ['GET', 'POST'])
+# def train_file():     
+#    if request.method == 'POST':
+
+#      return render_template('index.html', x1=files_grabbed1,x2=files_grabbed2)   # list files, delete files , and most importantly, do traning and generating new music!
+
+
 if __name__ == '__main__':
    app.run(debug = True)
 
+
+
+
+melody_rnn_create_dataset \
+--config='basic_rnn' \
+--input=/tmp/notesequences.tfrecord \
+--output_dir=/tmp/melody_rnn/sequence_examples \
+--eval_ratio=0.10
+
+
+melody_rnn_train \
+--config='basic_rnn' \
+--run_dir=/tmp/melody_rnn/logdir/run1 \
+--sequence_example_file=/tmp/melody_rnn/sequence_examples/training_melodies.tfrecord \
+--hparams="batch_size=64,rnn_layer_sizes=[64,64]" \
+--num_training_steps=20000
